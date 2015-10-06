d959a690ff4626a60016ad8ba48cd5a6b2d566a8
Valery Brezhnieva <angietheowl@gmail.com>
2015-10-06 23:12:25 +0300

    PersonalInfo is ready; Order is on work

--- Solution/ContosoProject/Domain/Entities/Order.cs
+++ Solution/ContosoProject/Domain/Entities/Order.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Order
+    public class Order : BaseEntity
     {
+        private Customer customer;
+        private List<GoodsRow> goodsList = new List<GoodsRow>();
+        private double totalCost;
+        private OrderStatus currentStatus;
+        private List<Comment> comments;
+
+        public Order(Customer currentCustomer, List<GoodsRow> good) : this (currentCustomer, good, new Comment())
+        {
+        }
+
+        public Order(Customer customer, List<GoodsRow> goods, Comment comment)
+        {
+            this.customer = customer;
+            if (goods != null)
+            {
+                foreach (GoodsRow row in goods)
+                {
+                    goodsList.Add(row);
+                    totalCost += row.Price;
+                }
+            }
+
+            if (comment.Message.Lenght != 0)
+            {
+                comments.Add(comment);
+            }
+        }
+
+        public void EditOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeactivateOrder ()
+        {
+            if (IsActive)
+                IsActive = false;
+        }
     }
 }
--- Solution/ContosoProject/Domain/Entities/PersonalInfo.cs
+++ Solution/ContosoProject/Domain/Entities/PersonalInfo.cs
@@ -8,5 +8,51 @@ namespace Domain.Entities
 {
     public class PersonalInfo
     {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length != 0)
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set
+            {
+                if (value.Length != 0)
+                {
+                    middleName = value;
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value.Length != 0)
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        public PersonalInfo (string name, string middleName, string surname)
+        {
+            FirstName = name;
+            MiddleName = middleName;
+            LastName = surname;
+        }
     }
 }
