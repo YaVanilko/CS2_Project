9ea95697eb671f799a99ac6904348518b46a1f74
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-10-07 15:55:19 +0300

    Four classes were implemented: BaseEntity, ContactInfo, Customer, PersonalInfo

--- Solution/ContosoProject/Domain/Entities/BaseEntity.cs
+++ Solution/ContosoProject/Domain/Entities/BaseEntity.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
+        static int uniqueCode = 0;
+        public int Id { get; protected set; }
+        public bool IsActive { get; protected set; }
+        public User Operator { get; protected set; }
+        public DateTime EditTime { get; protected set; }
+
+        public BaseEntity(User user)
+        {
+            Id = GetFreeCode();
+            IsActive = true;
+            Operator = user;
+            EditTime = DateTime.Now;
+        }
+        public static int GetFreeCode()
+        {
+            return ++uniqueCode;
+        }
     }
 }
--- Solution/ContosoProject/Domain/Entities/Customer.cs
+++ Solution/ContosoProject/Domain/Entities/Customer.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Customer
+    public class Customer: BaseEntity
     {
+        public PersonalInfo PersonalInfo { get; set; }
+        public ContactInfo Contacts { get; set; }
+        public List<Order> orders;
+        List<Comment> comments;
+        public Customer(User user, PersonalInfo personalInfo, ContactInfo contacts, Order order,
+            string city, string adress, string telephone, string firstName, string middleName, string lastName)
+            :base(user)
+        {
+            PersonalInfo = new PersonalInfo(user, firstName, middleName, lastName);
+            Contacts = new ContactInfo(user, city, adress, telephone);
+            orders.Add(new Order());
+            comments = new List<Comment>();
+        }
+        public void AddCustomer()
+        {
+
+        }
+        public void EditCustomer()
+        {
+
+        }
+        public void DeactivationCustomer()
+        {
+            IsActive = false;
+        }
+        public void AddComment()
+        {
+            comments.Add(new Comment());
+        }
+        public void AddOrder()
+        {
+            orders.Add(new Order());
+        }
     }
 }
