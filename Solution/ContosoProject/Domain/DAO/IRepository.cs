5c2a3866ffe6b93f0d1d121bbb85fb2fc089ec43
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-10-11 20:43:45 +0300

    Interfaces were implemented

---
+++ Solution/ContosoProject/Domain/DAO/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DAO
+{
+    interface IRepository<T>
+    {
+        public void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int Id); 
+        IEnumerable<T> GetAll();
+    }
+}
