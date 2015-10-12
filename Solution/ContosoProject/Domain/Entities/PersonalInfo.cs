6e37bd384491f73561c6c048c8eba812bd201139
vbre <angietheowl@gmail.com>
2015-10-12 18:35:35 +0300

    Delete ctors + added IPermissionRepository

--- Solution/ContosoProject/Domain/Entities/PersonalInfo.cs
+++ Solution/ContosoProject/Domain/Entities/PersonalInfo.cs
@@ -11,12 +11,5 @@ namespace Domain.Entities
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-
-        public PersonalInfo(User user, string firstName, string middleName, string lastName)
-        {
-            FirstName = firstName;
-            MiddleName = middleName;
-            LastName = lastName;
-        }
     }
 }
