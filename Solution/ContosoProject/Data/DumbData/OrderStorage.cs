b17fdaf04d58853c86efe2917679c3b903e5025e
exphoenix666 <smolenskyk@gmail.com>
2015-10-12 13:14:02 +0300

    temp commit

---
+++ Solution/ContosoProject/Data/DumbData/OrderStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Data.DumbData
+{
+    public static partial class Storage
+    {
+        public static List<Order> orderCollection = AddTenOrders();
+
+        private static List<Order> AddTenOrders()
+        {
+ 	        List<Order> orderCollection = new List<Order>();
+            
+            for (int i = 0; i < 9; i++)
+			{
+			    orderCollection.Add(new Order(customerCollection(i), new List<GoodsRow>(), new Comment("message"+i, CommentType.Order)));
+                orderCollection[i].goodsList.Add(goodsRowCollection[i]);
+			}
+            return orderCollection;
+        }
+
+    }
+}
---
+++ Solution/ContosoProject/Data/DumbData/UserStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Data.DumbData
+{
+    public static partial class Storage 
+    {
+        public static List<User> userCollection=AddTenUsers();
+
+        private static List<User> AddTenUsers()
+        {
+            List<User> userCollection = new List<User>();
+
+            for (int i = 0; i < 9; i++)
+            {
+                User user = new User(new PersonalInfo(null, "firstName" + i, "middleName" + i, "lastName" + i), "user" + i, "user" + i, new Role());
+                user.Operator = user;
+            }
+            return userCollection;
+        }
+    }
+}
