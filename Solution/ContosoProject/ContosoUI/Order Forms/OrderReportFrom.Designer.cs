d5e66613bd076312052398971a2d9c8e5cd6ed6d
Valery Brezhnieva <angietheowl@gmail.com>
2015-10-14 18:47:06 +0300

    ViewOrderFoorm, EditOrderForm, OrderReportForm

---
+++ Solution/ContosoProject/ContosoUI/Order Forms/OrderReportFrom.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace ContosoUI.Order_Forms
+{
+    partial class OrderReportFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTabControl = new System.Windows.Forms.TabControl();
+            this.customerTabPage = new System.Windows.Forms.TabPage();
+            this.statusTabPage = new System.Windows.Forms.TabPage();
+            this.goodTabPage = new System.Windows.Forms.TabPage();
+            this.customerLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.statusComboBox = new System.Windows.Forms.ComboBox();
+            this.customerComboBox = new System.Windows.Forms.ComboBox();
+            this.goodComboBox = new System.Windows.Forms.ComboBox();
+            this.goodLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerDataGridView = new System.Windows.Forms.DataGridView();
+            this.statusDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.mainTabControl.SuspendLayout();
+            this.customerTabPage.SuspendLayout();
+            this.statusTabPage.SuspendLayout();
+            this.goodTabPage.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.customerDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.statusDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Controls.Add(this.customerTabPage);
+            this.mainTabControl.Controls.Add(this.statusTabPage);
+            this.mainTabControl.Controls.Add(this.goodTabPage);
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 24);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(784, 537);
+            this.mainTabControl.TabIndex = 0;
+            // 
+            // customerTabPage
+            // 
+            this.customerTabPage.Controls.Add(this.customerDataGridView);
+            this.customerTabPage.Controls.Add(this.customerComboBox);
+            this.customerTabPage.Controls.Add(this.customerLabel);
+            this.customerTabPage.Location = new System.Drawing.Point(4, 22);
+            this.customerTabPage.Name = "customerTabPage";
+            this.customerTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.customerTabPage.Size = new System.Drawing.Size(776, 511);
+            this.customerTabPage.TabIndex = 0;
+            this.customerTabPage.Text = "Orders by customer";
+            this.customerTabPage.UseVisualStyleBackColor = true;
+            // 
+            // statusTabPage
+            // 
+            this.statusTabPage.Controls.Add(this.statusDataGridView);
+            this.statusTabPage.Controls.Add(this.statusComboBox);
+            this.statusTabPage.Controls.Add(this.statusLabel);
+            this.statusTabPage.Location = new System.Drawing.Point(4, 22);
+            this.statusTabPage.Name = "statusTabPage";
+            this.statusTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.statusTabPage.Size = new System.Drawing.Size(776, 511);
+            this.statusTabPage.TabIndex = 1;
+            this.statusTabPage.Text = "Orders by status";
+            this.statusTabPage.UseVisualStyleBackColor = true;
+            // 
+            // goodTabPage
+            // 
+            this.goodTabPage.Controls.Add(this.dataGridView3);
+            this.goodTabPage.Controls.Add(this.goodComboBox);
+            this.goodTabPage.Controls.Add(this.goodLabel);
+            this.goodTabPage.Location = new System.Drawing.Point(4, 22);
+            this.goodTabPage.Name = "goodTabPage";
+            this.goodTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.goodTabPage.Size = new System.Drawing.Size(776, 511);
+            this.goodTabPage.TabIndex = 2;
+            this.goodTabPage.Text = "Orders by good";
+            this.goodTabPage.UseVisualStyleBackColor = true;
+            // 
+            // customerLabel
+            // 
+            this.customerLabel.AutoSize = true;
+            this.customerLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerLabel.Location = new System.Drawing.Point(8, 29);
+            this.customerLabel.Name = "customerLabel";
+            this.customerLabel.Size = new System.Drawing.Size(131, 18);
+            this.customerLabel.TabIndex = 0;
+            this.customerLabel.Text = "Filter by customer";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(8, 29);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(108, 18);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Filter by status";
+            // 
+            // statusComboBox
+            // 
+            this.statusComboBox.FormattingEnabled = true;
+            this.statusComboBox.Location = new System.Drawing.Point(169, 26);
+            this.statusComboBox.Name = "statusComboBox";
+            this.statusComboBox.Size = new System.Drawing.Size(197, 21);
+            this.statusComboBox.TabIndex = 3;
+            // 
+            // customerComboBox
+            // 
+            this.customerComboBox.FormattingEnabled = true;
+            this.customerComboBox.Location = new System.Drawing.Point(169, 26);
+            this.customerComboBox.Name = "customerComboBox";
+            this.customerComboBox.Size = new System.Drawing.Size(197, 21);
+            this.customerComboBox.TabIndex = 5;
+            // 
+            // goodComboBox
+            // 
+            this.goodComboBox.FormattingEnabled = true;
+            this.goodComboBox.Location = new System.Drawing.Point(169, 26);
+            this.goodComboBox.Name = "goodComboBox";
+            this.goodComboBox.Size = new System.Drawing.Size(197, 21);
+            this.goodComboBox.TabIndex = 5;
+            // 
+            // goodLabel
+            // 
+            this.goodLabel.AutoSize = true;
+            this.goodLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goodLabel.Location = new System.Drawing.Point(8, 29);
+            this.goodLabel.Name = "goodLabel";
+            this.goodLabel.Size = new System.Drawing.Size(103, 18);
+            this.goodLabel.TabIndex = 4;
+            this.goodLabel.Text = "Filter by good";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editOrderToolStripMenuItem,
+            this.deleteOrderToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // editOrderToolStripMenuItem
+            // 
+            this.editOrderToolStripMenuItem.Name = "editOrderToolStripMenuItem";
+            this.editOrderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.editOrderToolStripMenuItem.Text = "Edit order";
+            // 
+            // deleteOrderToolStripMenuItem
+            // 
+            this.deleteOrderToolStripMenuItem.Name = "deleteOrderToolStripMenuItem";
+            this.deleteOrderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteOrderToolStripMenuItem.Text = "Delete order";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // customerDataGridView
+            // 
+            this.customerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.customerDataGridView.Location = new System.Drawing.Point(11, 73);
+            this.customerDataGridView.Name = "customerDataGridView";
+            this.customerDataGridView.Size = new System.Drawing.Size(727, 430);
+            this.customerDataGridView.TabIndex = 6;
+            // 
+            // statusDataGridView
+            // 
+            this.statusDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.statusDataGridView.Location = new System.Drawing.Point(11, 73);
+            this.statusDataGridView.Name = "statusDataGridView";
+            this.statusDataGridView.Size = new System.Drawing.Size(727, 430);
+            this.statusDataGridView.TabIndex = 7;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(11, 73);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(727, 430);
+            this.dataGridView3.TabIndex = 7;
+            // 
+            // OrderReportFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.mainTabControl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "OrderReportFrom";
+            this.Text = "Order report";
+            this.mainTabControl.ResumeLayout(false);
+            this.customerTabPage.ResumeLayout(false);
+            this.customerTabPage.PerformLayout();
+            this.statusTabPage.ResumeLayout(false);
+            this.statusTabPage.PerformLayout();
+            this.goodTabPage.ResumeLayout(false);
+            this.goodTabPage.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.customerDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.statusDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl mainTabControl;
+        private System.Windows.Forms.TabPage customerTabPage;
+        private System.Windows.Forms.TabPage statusTabPage;
+        private System.Windows.Forms.TabPage goodTabPage;
+        private System.Windows.Forms.Label customerLabel;
+        private System.Windows.Forms.ComboBox statusComboBox;
+        private System.Windows.Forms.ComboBox customerComboBox;
+        private System.Windows.Forms.ComboBox goodComboBox;
+        private System.Windows.Forms.Label goodLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.DataGridView customerDataGridView;
+        private System.Windows.Forms.DataGridView statusDataGridView;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        internal System.Windows.Forms.Label statusLabel;
+    }
+}
