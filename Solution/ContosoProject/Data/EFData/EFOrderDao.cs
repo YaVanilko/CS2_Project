a37cdb69fa5893c9d0e6a68fe8555dcb71b171bc
Valery Brezhnieva <angietheowl@gmail.com>
2015-10-20 22:31:52 +0300

    DAOs

---
+++ Solution/ContosoProject/Data/EFData/EFOrderDao.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DAO;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EFData
+{
+    public class EFOrderDao: EfBaseDao<Order>, IOrderRepository
+    {
+    }
+}
