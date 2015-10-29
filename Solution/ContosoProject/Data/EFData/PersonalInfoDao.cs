3b0d40a3be5c2768ad8120de95856eb78abcc5aa
exphoenix666 <smolenskyk@gmail.com>
2015-10-30 01:03:19 +0200

    add all necessary classes

---
+++ Solution/ContosoProject/Data/EFData/PersonalInfoDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DAO;
+using Domain.Entities;
+
+namespace Data.EFData
+{
+    public class PersonalInfoDao : EfBaseDao<PersonalInfo>, IPersonalInfoRepository
+    {
+    }
+}
