cb85b36e40b169e47da51d936343581a2155274e
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-11-02 19:42:26 +0200

    ProductCategoryView was added

---
+++ Solution/ContosoProject/ContosoUI/ProductCategoryList/ProductCategoryView.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ContosoUI.ProductCategoryList
+{
+    partial class ProductCategoryView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.saveAndNew = new DevExpress.XtraBars.BarButtonItem();
+            this.saveButton = new DevExpress.XtraBars.BarButtonItem();
+            this.mainRibbonPage = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.saveRibbonPageGroup = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.productCategoryGridControl = new DevExpress.XtraGrid.GridControl();
+            this.productCategoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colCategoryName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colIsActive = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEditTime = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.productCategoryControlItem = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryGridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryControlItem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.saveAndNew,
+            this.saveButton});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 3;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.mainRibbonPage});
+            this.ribbonControl1.Size = new System.Drawing.Size(805, 141);
+            // 
+            // saveAndNew
+            // 
+            this.saveAndNew.Caption = "Добавить";
+            this.saveAndNew.Id = 1;
+            this.saveAndNew.ImageUri.Uri = "SaveAndNew";
+            this.saveAndNew.Name = "saveAndNew";
+            this.saveAndNew.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.saveAndNew_ItemClick);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Caption = "Сохранить";
+            this.saveButton.Id = 2;
+            this.saveButton.ImageUri.Uri = "Save";
+            this.saveButton.Name = "saveButton";
+            this.saveButton.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.saveButton_ItemClick);
+            // 
+            // mainRibbonPage
+            // 
+            this.mainRibbonPage.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.saveRibbonPageGroup});
+            this.mainRibbonPage.Name = "mainRibbonPage";
+            this.mainRibbonPage.Text = "Меню";
+            // 
+            // saveRibbonPageGroup
+            // 
+            this.saveRibbonPageGroup.ItemLinks.Add(this.saveAndNew);
+            this.saveRibbonPageGroup.ItemLinks.Add(this.saveButton);
+            this.saveRibbonPageGroup.MergeOrder = 5;
+            this.saveRibbonPageGroup.Name = "saveRibbonPageGroup";
+            this.saveRibbonPageGroup.Text = "Сохранение";
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.productCategoryGridControl);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 141);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(805, 358);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // productCategoryGridControl
+            // 
+            this.productCategoryGridControl.DataSource = this.productCategoryBindingSource;
+            this.productCategoryGridControl.Location = new System.Drawing.Point(12, 28);
+            this.productCategoryGridControl.MainView = this.gridView1;
+            this.productCategoryGridControl.MenuManager = this.ribbonControl1;
+            this.productCategoryGridControl.Name = "productCategoryGridControl";
+            this.productCategoryGridControl.Size = new System.Drawing.Size(781, 318);
+            this.productCategoryGridControl.TabIndex = 4;
+            this.productCategoryGridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // productCategoryBindingSource
+            // 
+            this.productCategoryBindingSource.DataSource = typeof(Domain.Entities.ProductCategory);
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colCategoryName,
+            this.colId,
+            this.colIsActive,
+            this.colEditTime});
+            this.gridView1.GridControl = this.productCategoryGridControl;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colCategoryName
+            // 
+            this.colCategoryName.FieldName = "CategoryName";
+            this.colCategoryName.Name = "colCategoryName";
+            this.colCategoryName.Visible = true;
+            this.colCategoryName.VisibleIndex = 0;
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            this.colId.Visible = true;
+            this.colId.VisibleIndex = 1;
+            // 
+            // colIsActive
+            // 
+            this.colIsActive.FieldName = "IsActive";
+            this.colIsActive.Name = "colIsActive";
+            this.colIsActive.Visible = true;
+            this.colIsActive.VisibleIndex = 2;
+            // 
+            // colEditTime
+            // 
+            this.colEditTime.FieldName = "EditTime";
+            this.colEditTime.Name = "colEditTime";
+            this.colEditTime.Visible = true;
+            this.colEditTime.VisibleIndex = 3;
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.productCategoryControlItem});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(805, 358);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // productCategoryControlItem
+            // 
+            this.productCategoryControlItem.Control = this.productCategoryGridControl;
+            this.productCategoryControlItem.Location = new System.Drawing.Point(0, 0);
+            this.productCategoryControlItem.Name = "productCategoryControlItem";
+            this.productCategoryControlItem.Size = new System.Drawing.Size(785, 338);
+            this.productCategoryControlItem.Text = "Список категорий товаров";
+            this.productCategoryControlItem.TextLocation = DevExpress.Utils.Locations.Top;
+            this.productCategoryControlItem.TextSize = new System.Drawing.Size(137, 13);
+            // 
+            // ProductCategoryView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 499);
+            this.Controls.Add(this.layoutControl1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "ProductCategoryView";
+            this.Text = "Категории товаров";
+            this.Load += new System.EventHandler(this.ProductCategoryView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryGridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productCategoryControlItem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup saveRibbonPageGroup;
+        private DevExpress.XtraBars.BarButtonItem saveAndNew;
+        private DevExpress.XtraBars.Ribbon.RibbonPage mainRibbonPage;
+        private DevExpress.XtraBars.BarButtonItem saveButton;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraGrid.GridControl productCategoryGridControl;
+        private System.Windows.Forms.BindingSource productCategoryBindingSource;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colCategoryName;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colIsActive;
+        private DevExpress.XtraGrid.Columns.GridColumn colEditTime;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem productCategoryControlItem;
+    }
+}
