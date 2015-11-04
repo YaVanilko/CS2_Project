5cbcfcc8170f0c1eb1d46660de5c313a0382704a
exphoenix666 <smolenskyk@gmail.com>
2015-11-04 08:12:04 +0200

    fixes and refactoring
    fix password changing bug
    fix role changing bug
    add BasePresenter with INPC implementation

--- Solution/ContosoProject/ContosoUI/Roles/RolesPresenter.cs
+++ Solution/ContosoProject/ContosoUI/Roles/RolesPresenter.cs
@@ -1,4 +1,5 @@
-﻿using Data.EFData;
+﻿using ContosoUI.Presenter;
+using Data.EFData;
 using Domain.DAO;
 using Domain.Entities;
 using System;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace ContosoUI.Roles
 {
-    public class RolesPresenter : INotifyPropertyChanged
+    public class RolesPresenter : BasePresenter
     {
         readonly RolesView view;
         readonly RoleViewService modelProxy = new RoleViewService();
@@ -57,14 +58,7 @@ namespace ContosoUI.Roles
                 }
             }
         }
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(String info)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(info));
-            }
-        }
+
         public void Save()
         {
             foreach (var role in roles)
