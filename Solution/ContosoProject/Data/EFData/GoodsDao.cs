46f6a726b1fe8c1c8ade6cba4983e933c154d600
demmy <demiurge.ds@gmail.com>
2015-11-09 11:57:47 +0200

    GoodsDao refactor

--- Solution/ContosoProject/Data/EFData/GoodsDao.cs
+++ Solution/ContosoProject/Data/EFData/GoodsDao.cs
@@ -10,7 +10,7 @@ using System.Data.Entity.Migrations;
 
 namespace Data.EFData
 {
-    public class GoodsDao : IGoodsRepository
+    public class GoodsDao : EfBaseDao<Goods>, IGoodsRepository
     {
         public GoodsDao(ProjectContext context = null)
             : base(context)
@@ -20,42 +20,17 @@ namespace Data.EFData
 
         public ICollection<Goods> GetGoodsByCategory(string category)
         {
-            return dbContext.Products.Where(x => x.Category.CategoryName == category).
-                                      Include(x => x.Category).ToList();
+            return dbContext.Products.Where(x => x.Category.CategoryName == category).ToList();
         }
 
         public new ICollection<Goods> GetAll()
         {
-            return dbContext.Products.Include(x => x.Coments).
-                                      Include(x=>x.Category).ToList();
+            return dbContext.Products.Include(x => x.Coments).ToList();
         }
 
         public ICollection<Goods> GetAllIsActive()
         {
-            return dbContext.Products.Where(x => x.IsActive == true).
-                                      Include(x => x.Coments). 
-                                      Include(x=>x.Category).ToList();
-        }
-
-        public void Delete(Goods entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public new Goods GetById(int Id)
-        {
-            return dbContext.Products.Where(x => x.Id == Id). 
-                                      Include(x=>x.Category).FirstOrDefault();
-        }
-
-        IQueryable<Goods> IRepository<Goods>.GetAll()
-        {
-            return dbContext.Products;
-        }
-
-        public IQueryable<Goods> FindBy(System.Linq.Expressions.Expression<Func<Goods, bool>> predicate)
-        {
-            throw new NotImplementedException();
+            return dbContext.Products.Where(x => x.IsActive == true).Include(x => x.Coments).ToList();
         }
     }
 }
